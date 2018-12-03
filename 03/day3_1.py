eaea676e2612d279f11844d6015f7d6ae663d332
Bo0tzz <btdewilligen@gmail.com>
2018-12-03 11:34:57 +0100

    Day 3.2 solution

--- 03/day3_1.py
+++ 03/day3_1.py
@@ -1,13 +1,23 @@
 size = 1000
-sheet = [[0 for _ in range(size)] for _ in range(size)]  # Create a 1000x1000 list
+sheet = [[[] for _ in range(size)] for _ in range(size)]  # Create a 1000x1000 list
 
 
-def claim(s: list, x: int, y: int, width: int, height: int):
+def claim(s: list, number: int, x: int, y: int, width: int, height: int):
     for h in range(y, y + height):
         r = s[h]  # The row we operate on
         for w in range(x, x + width):
-            r[w] = r[w] + 1
+            r[w].append(number)
     return s
+
+
+def overlaps(s: list, number: int, x: int, y: int, width: int, height: int):
+    has_overlap = False
+    for h in range(y, y + height):
+        r = s[h]  # The row we operate on
+        for w in range(x, x + width):
+            if len(r[w]) > 1:
+                has_overlap = True
+    return has_overlap
 
 
 def parse(instruction: str):
@@ -15,14 +25,13 @@ def parse(instruction: str):
     coords, s = inst.split(':')
     x, y = coords.split(',')
     w, h = s.split('x')
-    return int(x), int(y), int(w), int(h)
+    return int(count[1:]), int(x), int(y), int(w), int(h)
 
 
 with open('in.txt') as puzzle_input:
-    for line in puzzle_input.readlines():
-        claim(sheet, *parse(line))
-    final_sum = 0
-    for row in sheet:
-        for spot in row:
-            final_sum += 1 if spot > 1 else 0
-    print(final_sum)
+    instructions = [parse(line) for line in puzzle_input.readlines()]
+    for i in instructions:
+        claim(sheet, *i)
+    for i in instructions:
+        if not overlaps(sheet, *i):
+            print(i[0])
